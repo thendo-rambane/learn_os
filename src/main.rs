afcce4001b1a8ca3daf5923c7b48d41bb9a41354
Thendo Rambane <personal@thendo.co.za>
2023-02-11 15:00:36 +0200

    Add basic vga driver

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
+use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
-
-    loop {}
+  loop {}
 }
 
 #[panic_handler]
-fn panic(_:&PanicInfo) -> ! {
-    loop {}
+fn panic(_: &PanicInfo) -> ! {
+  loop {}
 }
