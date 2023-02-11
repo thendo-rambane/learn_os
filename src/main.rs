e17b66be6169fd2d18f836b3e936e8d424b43bf5
Thendo Rambane <personal@thendo.co.za>
2023-02-11 15:21:17 +0200

    Expose global writer:
      * use `lazy_static` to expose global writer
        to kernel
      * use `spin::Mutex` to ensure writer is sync

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,17 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+  use core::fmt::Write;
+  vga_buffer::WRITER.lock().write_str("Hello Again").unwrap();
+  write!(
+    vga_buffer::WRITER.lock(),
+    ", some numbers: {} {}",
+    42,
+    1.337
+  )
+  .unwrap();
 
   loop {}
 }
