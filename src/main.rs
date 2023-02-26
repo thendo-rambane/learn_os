811499a6545d47c6b184e02337bd499741794d8b
Thendo Rambane <personal@thendo.co.za>
2023-02-26 18:22:44 +0200

    Pull `QemuExitCode` and `exit_qemu` into seperate file

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@
 
 mod serial;
 mod vga_buffer;
+mod qemu;
+
+use qemu::{QemuExitCode, exit_qemu};
 use core::panic::PanicInfo;
 
 #[no_mangle]
@@ -17,22 +20,6 @@ pub extern "C" fn _start() -> ! {
   test_main();
 
   loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-  Success = 0x10,
-  Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-  use x86_64::instructions::port::Port;
-
-  unsafe {
-    let mut port = Port::new(0xf4);
-    port.write(exit_code as u32)
-  }
 }
 
 #[cfg(test)]
