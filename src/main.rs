b0016188816cb7735761ff7b874fdf065f103d3f
Thendo Rambane <personal@thendo.co.za>
2023-02-26 15:44:53 +0200

    Add test harness:
      * Add testrunner and test_main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+  println!("Running {} tests", tests.len());
+  for test in tests {
+    test();
+  }
+}
+
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -8,7 +20,10 @@ use core::panic::PanicInfo;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
   println!("Hello World{}", "!");
-  panic!("Panicked Here");
+
+  #[cfg(test)]
+  test_main();
+
   loop {}
 }
 
@@ -16,4 +31,11 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
   println!("{}", info);
   loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+  print!("trivial assertion... ");
+  assert_eq!(1, 1);
+  println!("[ok]")
 }
